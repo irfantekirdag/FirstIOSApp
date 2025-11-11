3809cd247526e9a06d51c16910b791f7646b0b93
irfan <irfantekirdag@icloud.com>
2025-11-12 02:12:42 +0300

    Initial Commit

---
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  FirstIOSApp
+//
+//  Created by İrfan Tekirdağ on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
